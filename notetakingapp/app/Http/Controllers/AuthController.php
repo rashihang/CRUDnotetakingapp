66969f15f780032118c9cda1d1d42db73ddbf33a
Rashihang Rai <rashihangsm@gmail.com>
2025-09-04 10:31:35 +0545

    changes in authcontroller

--- notetakingapp/app/Http/Controllers/AuthController.php
+++ notetakingapp/app/Http/Controllers/AuthController.php
@@ -11,15 +11,33 @@ use App\Models\User;
 class AuthController extends Controller
 {
 
-    public function register(UserRegisterRequest $request){
-       $validateData = $request ->validated();
-       $user = User::create([
-           'name' => $validateData['name'],
-           'email' => $validateData['email'],
-           'password' => bcrypt($validateData['password']),
+    public function login(Request $request){
+        $validator = validator::make($request->all(),[
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6',
+        ]);
+        if ($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+        if (! $token = auth()->attempt($validator->validated())) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        return $this->createNewToken($token);
+    }
+
+    public function register(Request $request){
+       $validator = Validator::make($request->all(),[
+           'name' => 'required|string|max:255',
+           'email' => 'required|string|email|max:255|unique:users',
+           'password' => 'required|string|min:6',
        ]);
-       $token = auth()->login($user);
-       return $this->respondWithToken($token);
+       if($validator->fails()){
+           return response()->json($validator->errors(), 400);
+       }
+       $user = User::create(array_merge(
+           $validator->validated(),
+           ['password' => bcrypt($request->password)]
+       ));
 
    }
 
